bf4f8709081802e1388973f3a61a0466ed9582a1
virgilhg <120189340+virgilhg@users.noreply.github.com>
2025-06-13 15:43:21 -0500

    Portfolio is done

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
+
+// mobile nav toggle
+$(document).ready(function(){
+  $('.toggle').on('click', function(e){
+    e.preventDefault();
+    $('#navPanel').toggleClass('visible');
+    $('body').toggleClass('nav-open');
+  });
+
+  // close when a link is clicked
+  $('#navPanel a').on('click', function(){
+    $('#navPanel').removeClass('visible');
+    $('body').removeClass('nav-open');
+  });
+});
